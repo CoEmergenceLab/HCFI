8515df503dd270ca91418e031cc4259124336cfb
Carlos Castellanos <carloscastellanossf@gmail.com>
2017-03-08 17:07:36 -0600

    Add project files.

---
+++ src/ofApp.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"               // Include the main ofxOSC header 
+#include "ofxGrt.h"               // Include the main ofxGrt header
+#include "ofxKinectForWindows2.h" // Include KinectforWindows2 header
+
+using namespace GRT; // State that we want to use the GRT namespace
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+};
